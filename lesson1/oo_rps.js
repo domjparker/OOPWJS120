3db7561a659ad79386dc59612ac4405a9a56ff9b
Rees Dart <thedominicparker@gmail.com>
2021-04-15 15:07:21 -0700

    added suggestion capability for human player.

--- lesson1/oo_rps.js
+++ lesson1/oo_rps.js
@@ -1,14 +1,15 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = {
-  rock: ['rock', 'roc', 'ro', 'r'],
-  paper: ['paper', 'pape', 'pap', 'pa', 'p'],
-  scissors: ['scissors', 'scissor', 'scisso', 'sciss', 'scis', 'sci', 'sc'],
-  lizard: ['lizard', 'lizar', 'liza', 'liz', 'li', 'l'],
-  spock: ['spock', 'spoc', 'spo', 'sp'],
-};
-// const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+// const rules.validChoices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 let rules = {
+  validChoices: {
+    rock: ['rock', 'roc', 'ro', 'r'],
+    paper: ['paper', 'pape', 'pap', 'pa', 'p'],
+    scissors: ['scissors', 'scissor', 'scisso', 'sciss', 'scis', 'sci', 'sc'],
+    lizard: ['lizard', 'lizar', 'liza', 'liz', 'li', 'l'],
+    spock: ['spock', 'spoc', 'spo', 'sp'],
+  },
   pointsToWin: 1,
   winningChoices: {
     rock: ['scissors', 'lizard'],
@@ -25,9 +26,9 @@ let rules = {
 
 function createMoveMemory() {
   return {
-    timesChosen: 0,
-    timesWon: 0,
-    percentageWon: 0.00
+    timesOpponentChose: 0,
+    timesOpponentWon: 0,
+    percentageOpponentWon: 0.00
   };
 }
 
@@ -43,6 +44,20 @@ function createPlayer() {
       lizard: createMoveMemory(),
       spock: createMoveMemory(),
     },
+    // movePreferences: {
+    //   rock: 0,
+    //   paper: 0,
+    //   scissor: 0,
+    //   lizard: 0,
+    //   spock: 0
+    // },
+
+    calculateMovePreference(movePercentageWon) {
+      if (movePercentageWon > 0.90) return 3;
+      else if (movePercentageWon > 0.75) return 2;
+      else if (movePercentageWon > 0.60) return 1;
+      else return 0;
+    }
   };
 }
 
@@ -59,8 +74,8 @@ function createHuman() {
 
         let readlineInput = readline.question();
 
-        Object.keys(VALID_CHOICES).forEach(choiceKey => {
-          if (VALID_CHOICES[choiceKey].includes(readlineInput)) {
+        Object.keys(rules.validChoices).forEach(choiceKey => {
+          if (rules.validChoices[choiceKey].includes(readlineInput)) {
             this.move = choiceKey;
           }
         });
@@ -72,7 +87,8 @@ function createHuman() {
     choosePointsToWin() {
       let maxPointChoice;
       while (true) {
-        console.log(`Decide on a winning amount of points for the game. Choose a number: `);
+        console.log(`Decide how many points it will take for a player ` +
+          `to win the game. Choose a number: `);
         maxPointChoice = readline.question();
         if (!isNaN(maxPointChoice)) break;
       }
@@ -88,7 +104,7 @@ function createComputer() {
 
   let computerObject = {
     chooseMove() {
-      let keys = Object.keys(VALID_CHOICES);
+      let keys = Object.keys(rules.validChoices);
       this.move = keys[Math.floor(Math.random() * keys.length)];
     },
   };
@@ -116,18 +132,18 @@ const RPSGame = {
     let humanMove = this.human.move;
     let computerMove = this.computer.move;
 
-    if (rules.winningChoices[humanMove].includes(computerMove)) return "computer";
-    else if (rules.winningChoices[computerMove].includes(humanMove)) return "human";
+    if (rules.winningChoices[humanMove].includes(computerMove)) return "human";
+    else if (rules.winningChoices[computerMove].includes(humanMove)) return "computer";
     else return "tie";
   },
 
   displayRoundWinner(roundWinner) {
     if (roundWinner === "human") {
       console.log(`YOU won this round because ${this.human.move} beats ` +
-      `${this.computer.move}!`);
+        `${this.computer.move}!`);
     } else if (roundWinner === "computer") {
       console.log(`COMPUTER won this round because ${this.computer.move} ` +
-      `beats ${this.human.move}!`);
+        `beats ${this.human.move}!`);
     } else console.log(`It was a TIE!`);
   },
 
@@ -137,32 +153,56 @@ const RPSGame = {
   },
 
   updateMemory(roundWinner) {
-    this.human.memory[this.human.move]["timesChosen"] += 1;
-    this.computer.memory[this.computer.move]["timesChosen"] += 1;
+    this.human.memory[this.computer.move]["timesOpponentChose"] += 1;
+    this.computer.memory[this.human.move]["timesOpponentChose"] += 1;
 
     if (roundWinner === "human") {
-      this.human.memory[this.human.move]["timesWon"] += 1;
+      this.computer.memory[this.human.move]["timesOpponentWon"] += 1;
     } else if (roundWinner === "computer") {
-      this.computer.memory[this.computer.move]["timesWon"] += 1;
+      this.human.memory[this.computer.move]["timesOpponentWon"] += 1;
     }
 
-    this.human.memory[this.human.move]["percentageWon"] =
-    this.human.memory[this.human.move]["timesWon"] /
-    this.human.memory[this.human.move]["timesChosen"];
-    this.computer.memory[this.computer.move]["percentageWon"] =
-    this.computer.memory[this.computer.move]["timesWon"] /
-    this.computer.memory[this.computer.move]["timesChosen"];
+    this.human.memory[this.computer.move]["percentageOpponentWon"] =
+      this.human.memory[this.computer.move]["timesOpponentWon"] /
+      this.human.memory[this.computer.move]["timesOpponentChose"];
+    this.computer.memory[this.human.move]["percentageOpponentWon"] =
+      this.computer.memory[this.human.move]["timesOpponentWon"] /
+      this.computer.memory[this.human.move]["timesOpponentChose"];
     // if roundWinner is a tie, continue
   },
 
   displayScores() {
     console.log(`   |   Your Score: ${this.human.score}   |` +
-    `   Computer Score: ${this.computer.score}   |\n` +
-    `   ---------------------------------------------`);
+      `   Computer Score: ${this.computer.score}   |\n` +
+      `   ---------------------------------------------`);
   },
 
-  displayOverallWinner() {
-    if (this.human.score === 5) {
+  displayHumanSuggestion() {
+    // take percentageOpponentWon for each of the 5 choices, return list of choices as suggestions that would be preferable
+    // let choiceFavorability = [
+    //   ["rock", this.human.memory["rock"].percentageOpponentWon],
+    //   ["paper", this.human.memory["paper"].percentageOpponentWon],
+    //   ["scissors", this.human.memory["scissors"].percentageOpponentWon],
+    //   ["lizard", this.human.memory["lizard"].percentageOpponentWon],
+    //   ["spock", this.human.memory["spock"].percentageOpponentWon],
+    // ].sort((a, b) => {
+    //   if (a[1] > b[1]) {
+    //     return -1;
+    //   } else if (a[1] < b[1]) {
+    //     return 1;
+    //   }
+    //   return 0;
+    // });
+    // return choiceFavorability.map(subArr => subArr[0]);
+    console.log(`List of percentages for how often the computer wins when ` +
+    `choosing each move: \n`);
+    Object.keys(this.human.memory).forEach(key => {
+      console.log(`   ${key} : ${this.human.memory[key]["percentageOpponentWon"] * 100}%`);
+    });
+  },
+
+  displayOverallWinner(maxPoints) {
+    if (this.human.score === maxPoints) {
       console.log('You WON the overall Game!!!');
     } else console.log('You LOST! Computer won the overall game!!!');
   },
@@ -194,6 +234,7 @@ const RPSGame = {
       while (this.human.score < maxPoints && this.computer.score < maxPoints) {
         // console.clear();
         this.displayScores();
+        console.log(this.displayHumanSuggestion())
         this.human.chooseMove();
         this.computer.chooseMove();
         this.displayRoundMoves();
@@ -201,11 +242,11 @@ const RPSGame = {
         this.displayRoundWinner(roundWinner);
         this.updateMemory(roundWinner);
         this.updateRoundWinnerScore(roundWinner);
-        console.log(this.human.memory); // remove later
-        console.log(this.computer.memory); // remove later
+        // console.log(this.human.memory); // remove later
+        // console.log(this.computer.memory); // remove later
       }
 
-      this.displayOverallWinner();
+      this.displayOverallWinner(maxPoints);
       if (!this.playAgain()) break;
       else this.resetScores();
     }
